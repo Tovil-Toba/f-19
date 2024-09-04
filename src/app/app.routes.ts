54723ef0473f4fba9af8b70da54d21bfdf4355ca
Ilya Kondratiev <ikondratev@lanit.ru>
2024-09-05 01:39:22 +0300

    Ленивая загрузка для роутинга

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,57 @@
 import { Routes } from '@angular/router';
 
-import { DossierComponent } from './dossier/dossier.component';
-import { EncyclopediaComponent } from './encyclopedia/encyclopedia.component';
-import { GameComponent } from './game/game.component';
-import { MissionsHistoryComponent } from './missions-history/missions-history.component';
-import { NewsComponent } from './news/news.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { StoreComponent } from './store/store.component';
-import { UpgradesComponent } from './upgrades/upgrades.component';
-
 export const routes: Routes = [
-  { path: '', component: GameComponent },
-  { path: 'dossier', component: DossierComponent },
-  { path: 'encyclopedia/:id', component: EncyclopediaComponent },
-  { path: 'encyclopedia', component: EncyclopediaComponent },
-  { path: 'news', component: NewsComponent },
-  { path: 'missions-history', component: MissionsHistoryComponent },
-  { path: 'store', component: StoreComponent },
-  { path: 'upgrades', component: UpgradesComponent },
-  { path: '**', component: PageNotFoundComponent },
+  {
+    path: '',
+    loadComponent: () =>
+      import('./game/game.component').then((m) => m.GameComponent),
+  },
+  {
+    path: 'dossier',
+    loadComponent: () =>
+      import('./dossier/dossier.component').then((m) => m.DossierComponent),
+  },
+  {
+    path: 'encyclopedia/:id',
+    loadComponent: () =>
+      import('./encyclopedia/encyclopedia.component').then(
+        (m) => m.EncyclopediaComponent,
+      ),
+  },
+  {
+    path: 'encyclopedia',
+    loadComponent: () =>
+      import('./encyclopedia/encyclopedia.component').then(
+        (m) => m.EncyclopediaComponent,
+      ),
+  },
+  {
+    path: 'news',
+    loadComponent: () =>
+      import('./news/news.component').then((m) => m.NewsComponent),
+  },
+  {
+    path: 'missions-history',
+    loadComponent: () =>
+      import('./missions-history/missions-history.component').then(
+        (m) => m.MissionsHistoryComponent,
+      ),
+  },
+  {
+    path: 'store',
+    loadComponent: () =>
+      import('./store/store.component').then((m) => m.StoreComponent),
+  },
+  {
+    path: 'upgrades',
+    loadComponent: () =>
+      import('./upgrades/upgrades.component').then((m) => m.UpgradesComponent),
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./page-not-found/page-not-found.component').then(
+        (m) => m.PageNotFoundComponent,
+      ),
+  },
 ];
